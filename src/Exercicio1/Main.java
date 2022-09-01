f973b27dd2fc8c2d921e671d8616b0b21c738194
Filipe <filipeasm18@gmail.com>
2022-08-31 21:48:06 -0300

    Refactor: cleanned code

--- src/Exercicio1/Main.java
+++ src/Exercicio1/Main.java
@@ -2,22 +2,19 @@ package Exercicio1;
 
 import java.util.Scanner;
 
+/**
+ * Crie um programa que calcule a soma e o produto dos elementos de um vetor.
+ * Cada uma dessas operações deve ser executada por uma thread diferente.
+ */
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Digite um tamanho para o array: ");
-        int length = sc.nextInt();
-        int[] array = new int[length];
-
-        for (int i = 0; i < array.length; i++) {
-            array[i] = i;
-        }
+        int[] array = generateArray();
 
         MyThread t1 = new MyThread(array, 0); // Thread que executa a soma
         MyThread t2 = new MyThread(array, 1); // Thread que executa a produto
 
-        Thread th1 = new Thread(t1); // Threads superior a T1
-        Thread th2 = new Thread(t2); // Threas superior a T2
+        Thread th1 = new Thread(t1); // Thread container de T1
+        Thread th2 = new Thread(t2); // Thread container de T2
 
         th1.start();
         th2.start();
@@ -27,5 +24,22 @@ public class Main {
 
         System.out.println("Soma: " +t1.getResult());
         System.out.println("Produto: " +t2.getResult());
+    }
+
+    /**
+     * Gerando um array com tamanho definido pelo usuário
+     * @return novo array
+     */
+    public static int[] generateArray() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite um tamanho para o array: ");
+        int length = sc.nextInt();
+        int[] array = new int[length];
+
+        for (int i = 0; i < array.length; i++) { // inicializando o array
+            array[i] = i;
+        }
+
+        return array;
     }
 }
